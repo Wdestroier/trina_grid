9d5469afbba4bf1afeee2969afe3418f0b974ed8
Wagner Daimling Fontão <Wdestroier@gmail.com>
2025-05-23 16:41:58 -0300

    Merge cache extent changes

--- lib/src/trina_grid.dart
+++ lib/src/trina_grid.dart
@@ -82,6 +82,7 @@ class TrinaGrid extends TrinaStatefulWidget {
     super.key,
     required this.columns,
     required this.rows,
+    this.rowsCacheExtent,
     this.rowWrapper,
     this.editCellRenderer,
     this.columnGroups,
@@ -109,6 +110,8 @@ class TrinaGrid extends TrinaStatefulWidget {
     this.onValidationFailed,
     this.onLazyFetchCompleted,
   });
+
+  final double? rowsCacheExtent;
 
   /// {@macro trina_grid_row_wrapper}
   final RowWrapper? rowWrapper;
@@ -584,6 +587,7 @@ class TrinaGridState extends TrinaStateWithChange<TrinaGrid> {
         vertical: _verticalScroll,
         horizontal: _horizontalScroll,
       ),
+      rowsCacheExtent: widget.rowsCacheExtent,
       rowWrapper: widget.rowWrapper,
       editCellRenderer: widget.editCellRenderer,
       columnGroups: widget.columnGroups,
@@ -654,8 +658,7 @@ class TrinaGridState extends TrinaStateWithChange<TrinaGrid> {
     if (!widget.mode.isSelectMode) return;
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (_stateManager.configuration.enableAutoSelectFirstRow &&
-          _stateManager.currentCell == null) {
+      if (_stateManager.currentCell == null) {
         _stateManager.setCurrentCell(_stateManager.firstCell, 0);
       }
 
